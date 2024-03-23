4595ca67c6e6971b3283ba925e47fb1eadb0e7e8
Sanoscorb <sanoscorb@gmail.com>
2024-03-24 02:19:42 +0700

    Improved encapsulation, added highlight property

--- screen/screen.py
+++ screen/screen.py
@@ -1,7 +1,7 @@
 import curses
 import curses.panel as panel
 
-from .callback import *
+from .callback import Callback
 from .constants import *
 
 
@@ -33,12 +33,12 @@ class Screen:
     MENU_WIDTH = 15 + 2
     
     def __init__(self, names: list[str], callbacks: list[Callback]):
-        self.hlt = 0
-        self.pages = []
-        self.names = names
-        self.callbacks = callbacks
-        self.screen = curses.initscr()
-        self.height, self.width = self.screen.getmaxyx()
+        self.__hlt = 0
+        self.__pages = []
+        self.__names = names
+        self.__callbacks = callbacks
+        self.__screen = curses.initscr()
+        self.__height, self.__width = self.__screen.getmaxyx()
 
         cols = curses.COLS
         lines = curses.LINES
@@ -48,40 +48,48 @@ class Screen:
         curses.noecho()
         curses.curs_set(0)
         curses.set_escdelay(1)
-        self.screen.keypad(True)
+        self.__screen.keypad(True)
 
         for i in range(len(names)):
             win = curses.newwin(lines-2, cols-self.MENU_WIDTH-1, 1, self.MENU_WIDTH)
-            self.pages.append(panel.new_panel(win))
-            self.callbacks[i].window = win
+            self.__pages.append(panel.new_panel(win))
+            self.__callbacks[i].window = win
 
-        self.screen.box()
+        self.__screen.box()
         self.menu_border()
-        self.screen.addstr(0, self.MENU_WIDTH, names[self.hlt])
-        self.pages[self.hlt].top()
+        self.__screen.addstr(0, self.MENU_WIDTH, names[self.__hlt])
+        self.__pages[self.__hlt].top()
 
         panel.update_panels()
         curses.doupdate()
-        list_output(self.screen, 1, 1, names, -1)
+        list_output(self.__screen, 1, 1, names, -1)
 
     def __del__(self):
         curses.endwin()
 
-    def __getitem__(self, i):
-        return self.pages[i]
+    @property
+    def highlight(self):
+        return self.__hlt
     
+    @highlight.setter
+    def highlight(self, hlt):
+        if hlt < len(self.__names):
+            self.__hlt = hlt
+        
+        self.__pages[self.__hlt].top()
+
     def loop(self):
-        code = ExitCode.none
-        while code != ExitCode.endOfLoop:
-            ch = self.screen.getch()
+        code = ActionCode.NoAction
+        while code != ActionCode.EndOfLoop:
+            ch = self.__screen.getch()
             match ch:
                 case curses.KEY_RESIZE:
                     self.resize()
-                case 9:
+                case Keys.Tab:
                     self.switch()
                 case _:
                     curses.ungetch(ch)
-                    code = self.callbacks[self.hlt].action()
+                    code = self.__callbacks[self.__hlt].action()
 
             curses.flushinp()
 
@@ -90,48 +98,48 @@ class Screen:
         acs_btee = curses.ACS_BTEE
         acs_vline = curses.ACS_VLINE
 
-        self.screen.addch(0, self.MENU_WIDTH - 1, acs_ttee)
-        self.screen.vline(1, self.MENU_WIDTH - 1, acs_vline, self.height - 2)
-        self.screen.addch(self.height - 1, self.MENU_WIDTH - 1, acs_btee)
+        self.__screen.addch(0, self.MENU_WIDTH - 1, acs_ttee)
+        self.__screen.vline(1, self.MENU_WIDTH - 1, acs_vline, self.__height - 2)
+        self.__screen.addch(self.__height - 1, self.MENU_WIDTH - 1, acs_btee)
 
     def menu_process(self) -> int:
-        hlt = self.hlt
+        hlt = self.__hlt
         ch = 0
-        while ch not in [9, 13, 27]:
-            list_output(self.screen, 1, 1, self.names, hlt)
-            ch = self.screen.getch()
+        while ch not in [Keys.Enter, Keys.Escape, Keys.Tab]:
+            list_output(self.__screen, 1, 1, self.__names, hlt)
+            ch = self.__screen.getch()
 
             match ch:
                 case curses.KEY_UP:
                     if hlt > 0:
                         hlt -= 1
                 case curses.KEY_DOWN:
-                    if hlt < len(self.names) - 1:
+                    if hlt < len(self.__names) - 1:
                         hlt += 1
 
-        if ch == 13:
-            self.hlt = hlt
+        if ch == Keys.Enter:
+            self.__hlt = hlt
 
-        list_output(self.screen, 1, 1, self.names, -1)
+        list_output(self.__screen, 1, 1, self.__names, -1)
         return hlt
 
     def resize(self):
         try:
-            self.screen.hline(self.height - 1, 0, ' ', self.width)
-            self.screen.vline(0, self.width - 1, ' ', self.height)
-            self.screen.hline(0, 0, ' ', self.width)
-            self.screen.vline(0, 0, ' ', self.height)
+            self.__screen.hline(self.__height - 1, 0, ' ', self.__width)
+            self.__screen.vline(0, self.__width - 1, ' ', self.__height)
+            self.__screen.hline(0, 0, ' ', self.__width)
+            self.__screen.vline(0, 0, ' ', self.__height)
         except curses.error:
             pass
 
-        self.height, self.width = self.screen.getmaxyx()
-        curses.resizeterm(self.height, self.width)
-        self.screen.box()
+        self.__height, self.__width = self.__screen.getmaxyx()
+        curses.resizeterm(self.__height, self.__width)
+        self.__screen.box()
         self.menu_border()
-        self.screen.addstr(0, self.MENU_WIDTH, self.names[self.hlt])
+        self.__screen.addstr(0, self.MENU_WIDTH, self.__names[self.__hlt])
 
-        for page in self.pages:
-            page.window().resize(self.height-2, self.width-self.MENU_WIDTH-1)
+        for page in self.__pages:
+            page.window().resize(self.__height-2, self.__width-self.MENU_WIDTH-1)
 
         panel.update_panels()
         curses.doupdate()
@@ -139,15 +147,8 @@ class Screen:
     def switch(self):
         hline = curses.ACS_HLINE
 
-        self.screen.hline(0, self.MENU_WIDTH, hline, len(self.names[self.hlt]))
-        self.menu_process()
-        self.pages[self.hlt].top()
-        self.screen.addstr(0, self.MENU_WIDTH, self.names[self.hlt])
+        self.__screen.hline(0, self.MENU_WIDTH, hline, len(self.__names[self.__hlt]))
+        self.highlight = self.menu_process()
+        self.__screen.addstr(0, self.MENU_WIDTH, self.__names[self.__hlt])
         panel.update_panels()
         curses.doupdate()
-
-    def current(self):
-        return self.pages[self.hlt]
-
-    def highlight(self):
-        return self.hlt
